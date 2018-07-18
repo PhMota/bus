27e94e0c2dc8bdf0d6dd7bdc356e29a26a48ae9d
Philipe Mota <philipe.mota@gmail.com>
2018-07-17 21:19:47 -0300

    added ajax connection

--- mysite/bus/views.py
+++ mysite/bus/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+import time
 
 # Create your views here.
 def index(request):
     context = None
     return render(request, 'bus/index.html', context)
+
+
+def printAjax(request):
+    print 'printAjax', request.GET
+    data = {
+        'success': time.time(),
+    }
+    return JsonResponse(data)
